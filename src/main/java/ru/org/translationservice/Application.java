97c8e8eb78796a3947577020912031b4a4f76465
labisy <andreystepchenkov1@gmail.com>
2023-02-20 18:09:47 +0300

    add initDB.sql file and Application.java

--- src/main/java/ru/org/translationservice/Application.java
+++ src/main/java/ru/org/translationservice/Application.java
@@ -1,8 +1,12 @@
 
 package ru.org.translationservice;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-
+        SpringApplication.run(Application.class, args);
     }
 }
---
+++ src/main/java/ru/org/translationservice/entity/SentenceEntity.java
@@ -0,0 +1,4 @@
+package ru.org.translationservice.entity;
+
+public class SentenceEntity {
+}
